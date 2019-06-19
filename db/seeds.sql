04e0c8a550f274c1fe231ab28da143323c08c027
Jerne <Surajpatel527@gmail.com>
2019-06-19 13:28:37 -0400

    Changelog: Schema.sql and seeds.sql completed

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+### Populate data with 3 starter burgers
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burgers on Brioche Buns');
+
+INSERT INTO burgers (burger_name) VALUES ('Green-Chile Burgers with Fried Eggs');
+
+INSERT INTO burgers (burger_name) VALUES ('Angus burger');
