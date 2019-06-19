8a8b178f4059324f7865252829bd73e07770ab50
Suraj Patel <Surajpatel527@gmail.com>
2019-06-19 14:12:32 -0400

    Changelog: Tests completed, Working flawlessly looks ugly though.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 ### Populate data with 3 starter burgers
 INSERT INTO burgers (burger_name) VALUES ('Bacon Burgers on Brioche Buns');
 
-INSERT INTO burgers (burger_name) VALUES ('Green-Chile Burgers with Fried Eggs');
+INSERT INTO burgers (burger_name) VALUES ('Burger with Fried Eggs');
 
 INSERT INTO burgers (burger_name) VALUES ('Angus burger');
