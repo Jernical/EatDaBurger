b959a78f3e3e0ad0aafbbfab9e37234e2f0d18af
Jerne <Surajpatel527@gmail.com>
2019-06-19 13:41:14 -0400

    Changelog: Finished Server.js, index and main.handlebars, edited burgers.js.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,8 +4,8 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Our GET request to grab database contents
-router.get("/", function(req, res) {
-	burger.selectAll(function(data) {
+router.get("/", function (req, res) {
+	burger.selectAll(function (data) {
 		var hbsObj = {
 			burgers: data
 		};
@@ -15,20 +15,20 @@ router.get("/", function(req, res) {
 });
 
 // Our POST request to add a burger to the database
-router.post("/", function(req, res) {
+router.post("/", function (req, res) {
 	console.log(req.body.burger_name);
-	if(req.body.burger_name !== "") {
-		burger.insertOne(req.body.burger_name.trim(), function() {
+	if (req.body.burger_name !== "") {
+		burger.insertOne(req.body.burger_name.trim(), function () {
 			res.redirect("/");
 		});
 	}
 });
 
 // Our PUT request to update a burger's status
-router.put("/:id", function(req, res) {
+router.put("/:id", function (req, res) {
 	console.log(req.params.id);
 
-	burger.updateOne(req.params.id, function() {
+	burger.updateOne(req.params.id, function () {
 		res.redirect("/");
 	});
 })
